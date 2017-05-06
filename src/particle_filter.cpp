e66d91363d5f83e5a5e1ef27a7cca780b24b0b07
Hasan Korre <hkorre@gmail.com>
2017-05-06 22:32:53 +0000

    hkorre: working on getting new weights

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -46,8 +46,8 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   }
 
   is_initialized = true;
-  std::cout << "ParticleFilter initialized." << std::endl;
 
+  std::cout << "ParticleFilter initialized." << std::endl;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -87,20 +87,17 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 
   for (auto it_obs=observations.begin(); it_obs!=observations.end(); ++it_obs) {
     double min_dist_sq = START_DIST;
-    double min_x = 0;
-    double min_y = 0;
+    double min_id = 0;
 
     for (auto it_pred=predicted.begin(); it_pred!=predicted.end(); ++it_pred) {
       double dist_sq = dist(it_obs->x, it_obs->y, it_pred->x, it_pred->y);
       if (dist_sq < min_dist_sq) {
         min_dist_sq = dist_sq;
-        min_x = it_pred->x;
-        min_y = it_pred->y;
+        min_id = it_pred->id;
       }
     }
 
-    it_obs->x = min_x;
-    it_obs->y = min_y;
+    it_obs->id = min_id;
   }
 }
 
@@ -114,9 +111,23 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   //   The following is a good resource for the theory:
   //   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
   //   and the following is a good resource for the actual equation to implement (look at equation 
-  //   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
-  //   for the fact that the map's y-axis actually points downwards.)
+  //   3.33. 
   //   http://planning.cs.uiuc.edu/node99.html
+
+  // 1. Make list of all landmarks within sensor range of particle, call this `predicted_lm`
+
+
+  // 2. Convert all observations from local to global frame, call this `transformed_obs`
+
+
+  // 3. Perform `dataAssociation`. This will put the index of the `predicted_lm` nearest 
+  //    to each `transformed_obs` in the `id` field of the `transformed_obs` element.
+
+  // 4. Loop through all the `transformed_obs`. Use the saved index in the `id` to find 
+  //    the associated landmark and compute the gaussian. 
+
+  // 5. Multiply all the gaussian values together to get total probability of particle (the weight).
+
 
 
   //TODO: What to do with sensor_range?
@@ -124,22 +135,47 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
   double std_x = std_landmark[0];
   double std_y = std_landmark[1];
 
+  double range_sq = sensor_range*sensor_range;
+
+
 
   for (auto it_par=particles.begin(); it_par!=particles.end(); ++it_par) {
 
-    // transform each observation marker from the vehicle's coordinates to the map's coordinates
-    for (auto it_obs=observations.begin(); it_obs!=observations.end(); ++it_obs) {
-      //x = x*cos(theta) + y*sin(theta) + xt
-      //y = x*sin(theta) + y*cos(theta) + yt
+    // find landmarks in range of particle that sensor can reach...
+    std::vector<LandmarkObs> predicted;
+    for (auto it_map=map_landmarks.landmark_list.begin(); it_map!=map_landmarks.landmark_list.end(); ++it_map) {
+      double dist_sq = dist(it_map->x_f, it_map->y_f, it_par->x, it_par->y);
+      if (dist_sq < range_sq) {
+        struct LandmarkObs new_predicted = {
+          it_map->id_i,  //id
+          it_map->x_f,   //x
+          it_map->y_f    //y
+        };
+        predicted.push_back(new_predicted);
+      }
+    }
 
+    // transform each observations from vehicle frame to the map frame...
+    for (auto it_obs=observations.begin(); it_obs!=observations.end(); ++it_obs) {
+      double x = it_obs->x;
+      double y = it_obs->y;
+      double theta = it_par->theta;
+
+      //x_obsInMap = x_obsInCar*cos(theta_carInMap) - y_obsInCar*sin(theta_carInMap) + x_carInmap
+      it_obs->x = it_obs->x*cos(it_par->theta) - it_obs->y*sin(it_par->theta) + it_par->x;
+
+      //y_obsInMap = x_obsInCar*sin(theta_carInMap) + y_obsInCar*cos(theta_carInMap) + y_carInmap
+      it_obs->y = it_obs->x*sin(it_par->theta)  + it_obs->y*cos(it_par->theta) + it_par->y;
     }
 
     // associate each transformed observation with a land mark identifier
     //   use dataAssociation()
+    dataAssociation(predicted, observations);
 
     // calculate the particle's final weight
 
   }
+
   
 }
 
